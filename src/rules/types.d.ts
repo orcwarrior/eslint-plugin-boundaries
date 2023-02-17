072897356dbd9b5a635f40c2ab830b18e8e5bb72
Dariusz Kobuszewski <dariusz.kobuszewski@tdsoft.pl>
2023-02-17 17:17:24 +0100

    refactor(rules): entry-point.ts TS conversion, utility types.d.ts for all rules to use

---
+++ src/rules/types.d.ts
@@ -0,0 +1,17 @@
+import {ElementInfo, FileInfo} from "../core/elementsInfo";
+import {DependencyInfo} from "../core/dependencyInfo";
+import {RuleBoundariesBaseConfig} from "../configs/EslintPluginConfig";
+import {ElementRulesAllowDependencyResult} from "../helpers/rules";
+
+type RuleMatchingFunction = (
+  element: ElementInfo,
+  dependency: DependencyInfo,
+  options: RuleBoundariesBaseConfig) => ElementRulesAllowDependencyResult;
+
+type RuleErrorReporterFunction = (
+  ruleData: ElementRulesAllowDependencyResult,
+  file: FileInfo,
+  dependency: DependencyInfo
+) => string;
+
+export {RuleMatchingFunction, RuleErrorReporterFunction};
